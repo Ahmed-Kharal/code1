3dd0ac1c73dda167f18782d986fb44c907a39e1d
myexploreislam <myexploreislam@gmail.com>
2026-02-05 20:31:15 +0500

    Initial commit

---
+++ src/data/story.js
@@ -0,0 +1,28 @@
+import img1 from "../assets/images/1.jpeg";
+import img2 from "../assets/images/2.jpeg";
+import img3 from "../assets/images/3.jpeg";
+import img4 from "../assets/images/4.jpeg";
+import img5 from "../assets/images/5.jpeg";
+
+export const story = [
+  {
+    image: img1,
+    text: "Hiiii Goluuuu, Just wanted to tell you somethingggg...",
+  },
+  {
+    image: img2,
+    text: "Do you remember this picture? Our first picture that you took ✨",
+  },
+  {
+    image: img3,
+    text: "Our first trip as a hackathon... 🥺",
+  },
+  {
+    image: img4,
+    text: "A day full of enjoyymenttt 😌",
+  },
+  {
+    image: img5,
+    text: "Yeah, sometimes I do things that make you angry, and I am sorry for that. But just like this image, I choose US. So maaf kar de goluu ❤️",
+  },
+];
